f152dca0fc0fce4d56775aa85870cb7f65a7a894
nkchrnnk <vcernenkaa@yandex.ru>
2025-10-15 14:52:06 +0300

    Класс, представляющий книгу в библиотеке. Возвращает строковое представление книги

--- Program.cs
+++ Program.cs
@@ -30,3 +30,21 @@ public enum Genre
     Биография,
     История
 }
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public Genre Genre { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; } 
+    
+    public override string ToString()
+    {
+        return $"ID: {Id}, Название: {Title}, Автор: {Author}, Жанр: {Genre}, Год: {Year}, Цена: {Price:C}";
+    }
+}
+
+
+    
+    
