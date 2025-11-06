b818f5a84063e4cbe04d60ee78f1f1467d017075
nkchrnnk <vcernenkaa@yandex.ru>
2025-11-06 17:49:35 +0300

    Подключила бд, создала Core.cs для работы с бд. В Program.cs сделала поддержку русского языка и прописала старт игры.

---
+++ Entities/Core.cs
@@ -0,0 +1,9 @@
+using ChernenkayaV.V._523.Context;
+
+namespace ChernenkayaV.V._523
+{
+    public class Core
+    {
+        public static MyDbContext Context = new MyDbContext();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace ChernenkayaV.V._523
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            
+            new Game().Start();
+        }
+    }
+}
